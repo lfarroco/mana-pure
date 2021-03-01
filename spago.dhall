056ad61c521e57a8bf8f610d8c41d1aa136d8f86
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-03-01 18:59:29 -0300

    adds ref change

--- spago.dhall
+++ spago.dhall
@@ -9,6 +9,7 @@ You can edit this file as you like.
   , "effect"
   , "ordered-collections"
   , "psci-support"
+  , "refs"
   , "st"
   , "transformers"
   , "web-dom"
--- src/Main.purs
+++ src/Main.purs
@@ -2,66 +2,25 @@ module Main where
 
 import Prelude
 
-import Data.Map as M
-import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Graphics.Canvas (Context2D, fillPath, getCanvasElementById, getContext2D, rect, setFillStyle)
-import Partial.Unsafe (unsafePartial)
+import Effect.Ref as Ref
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.HTML (window)
 import Web.HTML.Event.EventTypes (click)
 import Web.HTML.Window (toEventTarget)
 
-data CharaName = CharaName { value:: String }
-createCharaName:: String -> CharaName
-createCharaName s = CharaName { value : s }
-
-data Age = Age { value:: String }
-createAge:: String -> Age
-createAge s = Age { value : s }
-
-type Chara = {
-  name :: CharaName,
-  age :: Age
-}
-
-createChara :: String -> String -> Chara
-createChara name age = { name: createCharaName name, age: createAge age } 
-
-data EventName = START | END
-derive instance eqEventName :: Eq EventName
-derive instance ordEventName :: Ord EventName
-
-index:: M.Map EventName String
-index = M.empty
-      # M.insert START "aaa"
-      # M.insert END "bbbb"
-
-emit:: EventName -> Effect Unit
-emit eventName =
-  case eventName of
-       START -> log "xaxxa"
-       END -> log "zazaza"
-
-square :: Context2D -> Effect Unit
-square ctx = rect ctx { x: 250.0 , y: 250.0 , width: 100.0 , height: 100.0 }
--- main :: Effect Unit
--- main = do
---   emit START
-
 main:: Effect Unit
-main = void $ unsafePartial do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
-  _ <- setFillStyle ctx "#0000FF"
-  fillPath ctx $ do
-     square ctx
- 
-  -- listen to clicks
-  target <- toEventTarget <$> window
+main = do
+  ref <- Ref.new 0
+  target <- map toEventTarget window
   clickListener <-
-    eventListener \e -> do log "hahaha"
+    eventListener \e -> do 
+      newVal <- Ref.modify (\n -> n + 1 ) ref
+      -- `modify_` updates the existing ref "in place"
+      -- without returning a new one
+      -- Ref.modify_ (\n -> n + 1 ) ref
+      log $ show newVal
   addEventListener click clickListener true target
   
 
