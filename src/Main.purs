441dbee27a2666cdb5d82828cee909894e173a75
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-03-02 14:18:59 -0300

    testing record update

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Ref as Ref
+import Game.Domain (initialGame, updateCounter)
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.HTML (window)
 import Web.HTML.Event.EventTypes (click)
@@ -12,11 +13,11 @@ import Web.HTML.Window (toEventTarget)
 
 main:: Effect Unit
 main = do
-  ref <- Ref.new 0
+  ref <- Ref.new initialGame
   target <- map toEventTarget window
   clickListener <-
     eventListener \e -> do 
-      newVal <- Ref.modify (\n -> n + 1 ) ref
+      newVal <- Ref.modify updateCounter ref
       -- `modify_` updates the existing ref "in place"
       -- without returning a new one
       -- Ref.modify_ (\n -> n + 1 ) ref
--- src/modules/Game/Domain/Game.purs
+++ src/modules/Game/Domain/Game.purs
@@ -1,23 +1,36 @@
 module Game.Domain where
 
+import Prelude
 import Core.BoundedNumber (BoundedNumber, createBoundedNumber)
 
 data ScreenName = MainScreen | UnitListScreen
+
+instance showScreenName  :: Show ScreenName where
+  show (MainScreen) =  show "MainScreen"
+  show (UnitListScreen) =  show "UnitListScreen"
 
 newtype Screen = Screen {
   name :: ScreenName,
   ui:: Int
 }
+instance showScreen  :: Show Screen where
+  show (Screen n) =  show n
 
 newtype Game = Game {
   currentScreen:: Screen,
   options:: Options,
-  uiScale:: BoundedNumber
+  uiScale:: BoundedNumber,
+  counter:: Int
 }
+instance showGame  :: Show Game where
+  show (Game n) =  show n
 
 data Options = Options {
   volume:: Volume
 }
+
+instance showOptions  :: Show Options where
+  show (Options n) = show n
 
 type Music = BoundedNumber
 type Audio = BoundedNumber
@@ -28,6 +41,8 @@ data Volume = Volume {
   audio:: Audio,
   general::General
 }
+instance showVolume  :: Show Volume where
+  show (Volume n) = show n
 
 initialVolume:: Volume
 initialVolume = Volume{
@@ -36,12 +51,30 @@ initialVolume = Volume{
   general: createBoundedNumber 0 100 100
 }
 
+createScreen:: ScreenName -> Int -> Screen
+createScreen name ui =  Screen {name, ui}
+
 mainScreen :: Screen
-mainScreen = Screen {name: MainScreen, ui: 22}
+mainScreen = createScreen MainScreen  22
 
 initialGame:: Game
 initialGame = Game {
   currentScreen: mainScreen,
   options: Options { volume: initialVolume },
-  uiScale: createBoundedNumber 0 100 50
+  uiScale: createBoundedNumber 0 100 50,
+  counter: 1
 }
+
+createGame :: Int -> Game
+createGame c = Game {
+  currentScreen: mainScreen,
+  options: Options { volume: initialVolume },
+  uiScale: createBoundedNumber 0 100 50,
+  counter: c
+}
+
+getCounter :: Game -> Int
+getCounter (Game g) = g.counter
+
+updateCounter :: Game -> Game
+updateCounter (Game g) = Game g { counter = g.counter + 1}
