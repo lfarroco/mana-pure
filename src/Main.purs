ae2ed59253266c231a7a46ce7da0e61a37591b30
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-03-04 14:22:09 -0300

    adds renderer

--- src/Main.purs
+++ src/Main.purs
@@ -5,41 +5,14 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Fiber, launchAff)
-import Effect.Class (class MonadEffect, liftEffect)
-import Effect.Console (log)
-import Graphics.Phaser (PhaserImage, PhaserScene, PhaserText, addImage, addTween, createScene, delay, imageOnPointerUp, newGame, setImageDisplaySize, setImageOrigin, text)
-import UI.Button (button)
-import UI.Elements (Element, render)
-
-
-createImageAt :: PhaserScene -> Effect PhaserImage
-createImageAt scene = do
-  img <- addImage scene 0 0 "backgrounds/sunset"
-  img_ <- setImageOrigin img 0 0
-  setImageDisplaySize img_ 800 600 
-
-write :: forall a. MonadEffect a => PhaserScene -> a PhaserText
-write scene = liftEffect $ text {
-      scene,
-      pos: {
-      x: 10,
-      y: 10},
-      text: "Aehoooo", config: { color: "#ffffff", fontSize: 24, fontFamily: "sans-serif" }
-      }
-
-bbb :: Element
-bbb = button "id" "Click me" {x:100,y: 100} {width:70, height: 30}
+import Effect.Class (liftEffect)
+import Graphics.Phaser (createScene, newGame)
+import Screen.MainScreen (mainScreen)
+import UI.Render (render)
 
 main :: Effect (Fiber Unit)
 main = do
   game <- newGame 800 600 
   launchAff do
     scene <- createScene game "main"
-    --btn <- liftEffect $ solidColorRect scene {x: 100, y: 100}  {width: 100, height: 100} "0xeaeaea" 
-    -- title <- write scene
-    -- image <- liftEffect $ createImageAt scene
-    -- imageOnPointerUp image
-    -- _ <-  liftEffect $ addTween scene image 1000 500 500 2000 "Cubic" 0 false 
-    -- delay scene 1000 
-    -- liftEffect <<< log $ "done!"
-    liftEffect $ render scene bbb Nothing
+    liftEffect $ render scene mainScreen Nothing
---
+++ src/modules/UI/Render.purs
@@ -0,0 +1,45 @@
+module UI.Render where
+
+import Prelude
+
+import Data.Foldable (for_)
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Graphics.Phaser (PhaserContainer, PhaserScene, 
+        addContainer, addGraphicsToContainer, addImage, addImageToContainer, 
+        addTextToContainer, solidColorRect, text)
+import UI.Elements (Element(..))
+
+
+addToContainer :: forall t11 t2 t5 t6.
+  Applicative t6 => Bind t6 => Maybe t5
+                               -> t2
+                                  -> ({ container :: t5
+                                      , element :: t2
+                                      }
+                                      -> t6 t11
+                                     )
+                                     -> t6 Unit
+addToContainer parent element fn = 
+        case parent of 
+            Nothing -> do
+                pure unit
+            Just container -> do  
+                _ <- fn {element, container}
+                pure unit
+
+render :: PhaserScene -> Element -> Maybe PhaserContainer -> Effect Unit
+render scene element parentContainer = 
+  case element of
+    Container c -> do
+        container <- addContainer scene c.pos.x c.pos.y
+        for_ c.children (\e->render scene e $ Just container) 
+    Image i -> do 
+        img <- addImage scene i.pos.x i.pos.y i.texture
+        addToContainer parentContainer img addImageToContainer
+    Rect r -> do
+        rect <- solidColorRect scene r.pos r.size r.color
+        addToContainer parentContainer rect addGraphicsToContainer
+    Text t -> do
+        text_ <- text {scene, pos: t.pos, text: t.text, config: {color: "#fff", fontSize: 18, fontFamily: "sans-serif"} }  
+        addToContainer parentContainer text_ addTextToContainer
