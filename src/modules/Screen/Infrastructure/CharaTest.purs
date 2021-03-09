f84bb96da50cc24cd0527ea0b128ead893775fb1
Leonardo Farroco <leonardo.farroco@gmail.com>
2021-03-09 20:10:18 -0300

    refactor with deconstructing

--- src/modules/Screen/Infrastructure/CharaTest.purs
+++ src/modules/Screen/Infrastructure/CharaTest.purs
@@ -2,7 +2,7 @@ module Screen.Infrastructure.CharaTest where
 
 import Prelude
 import Core.Models (size, vec)
-import Data.Array (filter, mapMaybe)
+import Data.Array (mapMaybe)
 import Data.Maybe (Maybe(..))
 import Game.Domain.Element (Element(..), createContainerId)
 import Game.Domain.Events (ManaEvent(..))
@@ -25,18 +25,18 @@ chara :: String -> Element
 chara id =
   let
     images =
-      [ { id: "chara/lhand/" <> id, texture: "chara/hand", origin: vec 125 130, target: Just $ vec 125 120, duration: 500 }
-      , { id: "chara/lfoot/" <> id, texture: "chara/foot", origin: vec 120 172, target: Nothing, duration: 0 }
-      , { id: "chara/rfoot/" <> id, texture: "chara/foot", origin: vec 90 180, target: Nothing, duration: 0 }
-      , { id: "chara/trunk/" <> id, texture: "chara/trunk_fighter", origin: vec 100 140, target: Just $ vec 100 137, duration: 500 }
-      , { id: "chara/head/" <> id, texture: "chara/head_male", origin: vec 100 70, target: Just $ vec 100 67, duration: 500 }
-      , { id: "chara/rhand/" <> id, texture: "chara/hand", origin: vec 80 140, target: Just $ vec 80 135, duration: 500 }
+      [ { id: "chara/lhand/" <> id, texture: "chara/hand", pos: vec 125 130, target: Just $ vec 125 120, duration: 500 }
+      , { id: "chara/lfoot/" <> id, texture: "chara/foot", pos: vec 120 172, target: Nothing, duration: 0 }
+      , { id: "chara/rfoot/" <> id, texture: "chara/foot", pos: vec 90 180, target: Nothing, duration: 0 }
+      , { id: "chara/trunk/" <> id, texture: "chara/trunk_fighter", pos: vec 100 140, target: Just $ vec 100 137, duration: 500 }
+      , { id: "chara/head/" <> id, texture: "chara/head_male", pos: vec 100 70, target: Just $ vec 100 67, duration: 500 }
+      , { id: "chara/rhand/" <> id, texture: "chara/hand", pos: vec 80 140, target: Just $ vec 80 135, duration: 500 }
       ]
 
     standAnimation =
       mapMaybe
-        ( \i -> case i.target of
-            Just t -> Just $ TweenImage i.id t i.duration
+        ( \{ id, target, duration } -> case target of
+            Just t -> Just $ TweenImage id t duration
             Nothing -> Nothing
         )
         images
@@ -49,8 +49,8 @@ chara id =
       , onCreate: standAnimation
       , children:
           map
-            ( \i ->
-                Image { id: i.id, pos: i.origin, texture: i.texture }
+            ( \{ id, pos, texture } ->
+                Image { id, pos, texture }
             )
             images
       }
